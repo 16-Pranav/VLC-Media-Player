8e92f78ae5248ec1d057dd924bdaa7f7935bb16b
16-Pranav <123pranavbhat@gmail.com>
2024-11-04 22:23:22 +0530

    Completed Code with all Functionalities

--- index.js
+++ index.js
@@ -5,21 +5,58 @@ const volumeDown = document.querySelector("#volumeDown");
 const videoBtn = document.querySelector("#videoBtn");
 const videoInput = document.querySelector("#videoInput");
 const videoPlayer = document.querySelector("#main");
+const toast = document.querySelector(".toast");
+
+const totalTimeElem = document.querySelector("#totalTime");
+const currentTimeElem = document.querySelector("#currentTime");
+const slider = document.querySelector("#slider");
+
+let video = "";
+let duration;
+let timerObj;
+let currentPlayTime = 0;
+let isPlaying = false;
 
 const handleInput = () => {
   videoInput.click();
 };
 const acceptInputHandler = (obj) => {
-  const selectedVideo = obj.target.files[0];
-
-  // Src -> base64
-
+  let selectedVideo;
+  // console.log(obj);
+  if (obj.type == "drop") {
+    selectedVideo = obj.dataTransfer.files[0];
+  } else {
+    selectedVideo = obj.target.files[0];
+  }
+  //  src -> base64
   const link = URL.createObjectURL(selectedVideo);
-  const video = document.createElement("video");
-  video.src = link;
-  video.controls = "true";
-  video.setAttribute("class", "video");
-  videoPlayer.appendChild(video);
+  // console.log(link);
+
+  const videoElement = document.createElement("video");
+  videoElement.src = link;
+  videoElement.setAttribute("class", "video");
+  // check if there are any video already present
+  if (videoPlayer.children.length > 0) {
+    // if present -> remove it
+    videoPlayer.removeChild(videoPlayer.children[0]);
+  }
+  // now after the above check -> add the videoElement
+  videoPlayer.appendChild(videoElement);
+
+  video = videoElement;
+  isPlaying = true;
+  setPlayPause();
+  videoElement.play();
+  videoElement.volume = 0.3;
+  videoElement.addEventListener("loadedmetadata", function () {
+    // it gives in decimal value -> convert that into seconds
+    duration = Math.round(videoElement.duration);
+    // convert seconds into hrs:mins:secs
+    let time = timeFormat(duration);
+    totalTimeElem.innerText = time;
+    slider.setAttribute("max", duration);
+    startTimer();
+  });
 };
 
 const speedUpHandler = () => {
@@ -29,8 +66,15 @@ const speedUpHandler = () => {
   if (videoElement == null) {
     return;
   }
+
+  if (videoElement.playbackRate > 3) {
+    return;
+  }
+
   const increaseSpeed = videoElement.playbackRate + 0.5;
   videoElement.playbackRate = increaseSpeed;
+
+  showToast(increaseSpeed + "X");
 };
 
 const speedDownHandler = () => {
@@ -41,8 +85,12 @@ const speedDownHandler = () => {
     return;
   }
 
-  const decreaseSpeed = videoElement.playbackRate - 0.5;
-  videoElement.playbackRate = decreaseSpeed;
+  if (videoElement.playbackRate > 0) {
+    const decreaseSpeed = videoElement.playbackRate - 0.5;
+    videoElement.playbackRate = decreaseSpeed;
+
+    showToast(decreaseSpeed + "X");
+  }
 };
 
 const volumeUpHandler = () => {
@@ -56,8 +104,12 @@ const volumeUpHandler = () => {
     return;
   }
 
-  videoElement.volume = videoElement.volume + 0.1;
-  console.log("increased volume by: ", videoElement.volume);
+  const increasedVolume = videoElement.volume + 0.1;
+  videoElement.volume = increasedVolume;
+  console.log("increased volume by: ", increasedVolume);
+
+  const percentage = Math.round(increasedVolume * 100);
+  showToast(percentage + "%");
 };
 
 const volumeDownHandler = () => {
@@ -72,9 +124,22 @@ const volumeDownHandler = () => {
     return;
   }
 
-  videoElement.volume = videoElement.volume - 0.1;
-  console.log("decreased volume by: ", videoElement.volume);
+  const decreasedVolume = videoElement.volume - 0.1;
+  videoElement.volume = decreasedVolume;
+  console.log("decreased volume by: ", decreasedVolume);
+
+  const percentage = Math.round(decreasedVolume * 100);
+  showToast(percentage + "%");
 };
+
+function showToast(message) {
+  // toast show
+  toast.textContent = message;
+  toast.style.display = "block";
+  setTimeout(() => {
+    toast.style.display = "none";
+  }, 1000);
+}
 videoBtn.addEventListener("click", handleInput);
 // When file is selected:
 videoInput.addEventListener("change", acceptInputHandler);
@@ -86,3 +151,157 @@ speedDown.addEventListener("click", speedDownHandler);
 volumeUp.addEventListener("click", volumeUpHandler);
 // volume decreaese:
 volumeDown.addEventListener("click", volumeDownHandler);
+
+// ************************controls**************************** //
+const fullScreen = document.querySelector("#fullScreen");
+
+const handleFullScreen = () => {
+  videoPlayer.requestFullscreen();
+};
+
+fullScreen.addEventListener("click", handleFullScreen);
+
+/***********forward and backward button*************/
+function forward() {
+  currentPlayTime = Math.round(video.currentTime) + 5;
+  video.currentTime = currentPlayTime;
+  slider.setAttribute("value", currentPlayTime);
+  showToast("Forward by 5 sec");
+  let time = timeFormat(currentPlayTime);
+  currentTimeElem.innerText = time;
+}
+
+function backward() {
+  currentPlayTime = Math.round(video.currentTime) - 5;
+  video.currentTime = currentPlayTime;
+  slider.setAttribute("value", currentPlayTime);
+  showToast("Backward by 5 sec");
+  let time = timeFormat(currentPlayTime);
+  currentTimeElem.innerText = time;
+}
+
+const forwardBtn = document.querySelector("#forwardBtn");
+const backwardBtn = document.querySelector("#backBtn");
+forwardBtn.addEventListener("click", forward);
+backwardBtn.addEventListener("click", backward);
+
+
+/****************play pause********************/
+const playPauseContainer = document.querySelector("#playPause");
+function setPlayPause() {
+  if (isPlaying === true) {
+    playPauseContainer.innerHTML = `<i class="fas fa-pause state"></i>`;
+    video.play();
+  } else {
+    playPauseContainer.innerHTML = `<i class="fas fa-play state"></i>`;
+    video.pause();
+  }
+}
+
+playPauseContainer.addEventListener("click", function (e) {
+  if (video) {
+    isPlaying = !isPlaying;
+    setPlayPause();
+  }
+});
+
+/******stop btn********/
+const stopBtn = document.querySelector("#stopBtn");
+const stopHandler = () => {
+  if (video) {
+    // remove the video from ui
+    video.remove();
+    // reset all the varibales
+    isPlaying = false;
+    currentPlayTime = 0;
+    slider.value = 0;
+    video = "";
+    duration = "";
+    totalTimeElem.innerText = "--/--";
+    currentTimeElem.innerText = "00:00";
+    slider.setAttribute("value", 0);
+    stopTimer();
+    setPlayPause();
+  }
+};
+
+stopBtn.addEventListener("click", stopHandler);
+
+/***************utility function to convert secs into hrs :mns : seconds*****************/
+function timeFormat(timeCount) {
+  let time = "";
+  const sec = parseInt(timeCount, 10);
+  let hours = Math.floor(sec / 3600);
+  let minutes = Math.floor((sec - hours * 3600) / 60);
+  let seconds = sec - hours * 3600 - minutes * 60;
+  if (hours < 10) hours = "0" + hours;
+  if (minutes < 10) minutes = "0" + minutes;
+  if (seconds < 10) seconds = "0" + seconds;
+  time = `${hours}:${minutes}:${seconds}`;
+  return time;
+}
+
+// function that runs the slider and timer
+function startTimer() {
+  timerObj = setInterval(function () {
+    currentPlayTime = Math.round(video.currentTime);
+    slider.value = currentPlayTime;
+    const time = timeFormat(currentPlayTime);
+    currentTimeElem.innerText = time;
+    if (currentPlayTime == duration) {
+      state = "pause";
+      stopTimer();
+      setPlayPause();
+      video.remove();
+      slider.value = 0;
+      currentTimeElem.innerText = "00:00:00";
+      totalTimeElem.innerText = "--/--/--";
+    }
+  }, 1000);
+}
+function stopTimer() {
+  clearInterval(timerObj);
+}
+
+/**********************enable drag and drop**********************/
+// Prevent default behavior for dragover and dragleave events
+videoPlayer.addEventListener("dragenter", (e) => {
+  e.preventDefault();
+});
+
+videoPlayer.addEventListener("dragover", (e) => {
+  e.preventDefault();
+});
+
+videoPlayer.addEventListener("dragleave", (e) => {
+  e.preventDefault();
+});
+
+videoPlayer.addEventListener("drop", (e) => {
+  e.preventDefault();
+  acceptInputHandler(e);
+});
+
+/*********keyboard support***************/
+const body = document.querySelector("body");
+// keyboard inputs
+body.addEventListener("keyup", function (e) {
+  console.log(e.key);
+  if (!video) return;
+  if (e.code == "Space") {
+    isPlaying = !isPlaying;
+    setPlayPause();
+  } else if (e.key == "ArrowUp") {
+    volumeUpHandler();
+  } else if (e.key == "ArrowDown") {
+    volumeDownHandler();
+  } else if (e.key == "+") {
+    speedUpHandler();
+  } else if (e.key == "-") {
+    speedDownhandler();
+  } else if (e.key == "ArrowRight") {
+    forward();
+  } else if (e.key == "ArrowLeft") {
+    backward();
+  }
+});
